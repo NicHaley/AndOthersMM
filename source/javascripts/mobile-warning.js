92d42038eaf013fe068d433834a4e2ac67d2122e
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-10 14:56:52 -0400

    Migrated the exile content from old project to middleman project

---
+++ source/javascripts/mobile-warning.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	if ( ($(window).width() < 768) && ($(window).height() > $(window).width())) {
+		setTimeout(function(){
+			$('.mobile-container').addClass("rotate");
+		}, 500);
+
+		setTimeout(function(){
+			$('.mobile').fadeOut("fast");
+		}, 5000);
+	} else {
+		$('.mobile').css("display", "none");
+	}
+});
---
+++ source/javascripts/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	setTimeout(function(){
+		$(".audio-ex")[0].loop = true;
+		$(".audio-ex")[0].play();
+
+		$('.icon').click(function() {
+			if ($(".audio-ex")[0].paused == false) {
+			    $(".audio-ex")[0].pause();
+			    // alert('music paused');
+			} else {
+			    $(".audio-ex")[0].play();
+			    // alert('music playing');
+			}
+		});
+	}, 500);
+});
