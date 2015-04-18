1b6ba615949ad122614f7b3f1fcef3e21d78f829
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-17 21:56:45 -0400

    Created JS countdown clock

---
+++ source/javascripts/home-timer.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	 endDate = new Date(2015, 04, 1, 00, 00, 00);
+
+	setInterval(function(){
+		thisDate  = new Date();
+		thisDate  = new Date(thisDate.getFullYear(), thisDate.getMonth(), thisDate.getDate(), thisDate.getHours(), thisDate.getMinutes(), thisDate.getSeconds(), 00, 00);
+
+		var daysLeft = parseInt((endDate-thisDate)/86400000);
+		var hoursLeft = parseInt((endDate-thisDate)/3600000); 
+		var minutsLeft = parseInt((endDate-thisDate)/60000);
+		var secondsLeft = parseInt((endDate-thisDate)/1000);
+
+		seconds = minutsLeft*60;
+		seconds = secondsLeft-seconds;
+
+		minutes = hoursLeft*60;
+		minutes = minutsLeft-minutes;
+
+		hours = daysLeft*24;
+		hours = (hoursLeft-hours) < 0 ? 0 : hoursLeft-hours;
+
+		days = daysLeft;
+
+		$("#time").html(days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds");
+	}, 1000);
+});
--- source/javascripts/timer.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	setInterval(function(){
-		
-	}, 1000);
-})
