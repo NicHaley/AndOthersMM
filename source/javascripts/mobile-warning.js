2f6ac589bf9d723299beb1b85fa7c28dab39fb1d
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-19 16:52:25 -0400

    comment js

--- source/javascripts/mobile-warning.js
+++ source/javascripts/mobile-warning.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-	if ( ($(window).width() < 768) && ($(window).height() > $(window).width())) {
-		setTimeout(function(){
-			$('.mobile-container').addClass("rotate");
-		}, 500);
+// $(document).ready(function(){
+// 	if ( ($(window).width() < 768) && ($(window).height() > $(window).width())) {
+// 		setTimeout(function(){
+// 			$('.mobile-container').addClass("rotate");
+// 		}, 500);
 
-		setTimeout(function(){
-			$('.mobile').fadeOut("fast");
-		}, 5000);
-	} else {
-		$('.mobile').css("display", "none");
-	}
-});
+// 		setTimeout(function(){
+// 			$('.mobile').fadeOut("fast");
+// 		}, 5000);
+// 	} else {
+// 		$('.mobile').css("display", "none");
+// 	}
+// });
