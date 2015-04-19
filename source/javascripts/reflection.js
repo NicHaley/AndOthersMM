842caf4a8ba378b87216a2c6e754fc416d219898
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-19 16:38:32 -0400

    Bug fix to commented out js

--- source/javascripts/reflection.js
+++ source/javascripts/reflection.js
@@ -1,23 +1,23 @@
-$(window).load(function(){
-	try {
-		$('.ec-bg').ripples({
-			resolution: 512,
-					dropRadius: 20, //px
-					perturbance: 0.04,
-				});
-	}
-	catch (e) {
-		$('.error').show().text(e);
-	}
+// $(window).load(function(){
+// 	try {
+// 		$('.ec-bg').ripples({
+// 			resolution: 512,
+// 					dropRadius: 20, //px
+// 					perturbance: 0.04,
+// 				});
+// 	}
+// 	catch (e) {
+// 		$('.error').show().text(e);
+// 	}
 
-	// Automatic drops
-	setInterval(function() {
-		var $el = $('.ec-bg');
-		var x = Math.random() * $el.outerWidth();
-		var y = Math.random() * $el.outerHeight();
-		var dropRadius = 20;
-		var strength = 0.03 + Math.random() * 0.03;
+// 	// Automatic drops
+// 	setInterval(function() {
+// 		var $el = $('.ec-bg');
+// 		var x = Math.random() * $el.outerWidth();
+// 		var y = Math.random() * $el.outerHeight();
+// 		var dropRadius = 20;
+// 		var strength = 0.03 + Math.random() * 0.03;
 		
-		$el.ripples('drop', x, y, dropRadius, strength);
-	}, 2000);
-});
+// 		$el.ripples('drop', x, y, dropRadius, strength);
+// 	}, 2000);
+// });
