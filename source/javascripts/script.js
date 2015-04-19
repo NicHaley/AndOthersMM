e8cfd16c5750e5437981dd543d43a2027b6fe36c
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-19 16:53:15 -0400

    Bug fix

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,21 +1,21 @@
-// $(document).ready(function(){
-// 	setTimeout(function(){
-// 		$(".audio-ex")[0].loop = true;
-// 		$(".audio-ex")[0].play();
+$(document).ready(function(){
+	setTimeout(function(){
+		$(".audio-ex")[0].loop = true;
+		$(".audio-ex")[0].play();
 
-// 		$('.icon').click(function() {
-// 			if ($(".audio-ex")[0].paused == false) {
-// 				$(".audio-ex")[0].pause();
-// 			    // alert('music paused');
-// 			} else {
-// 				$(".audio-ex")[0].play();
-// 			    // alert('music playing');
-// 			}
-// 		});
-// 	}, 500);
+		$('.icon').click(function() {
+			if ($(".audio-ex")[0].paused == false) {
+				$(".audio-ex")[0].pause();
+			    // alert('music paused');
+			} else {
+				$(".audio-ex")[0].play();
+			    // alert('music playing');
+			}
+		});
+	}, 500);
 
-// 	$('.poem-ex').selectionSharer();
-// 	$('.intro-quote').selectionSharer();
-// 	$('.poem-tr').selectionSharer();
-// 	$('.intro-tr').selectionSharer();
-// });
+	// $('.poem-ex').selectionSharer();
+	// $('.intro-quote').selectionSharer();
+	// $('.poem-tr').selectionSharer();
+	// $('.intro-tr').selectionSharer();
+});
