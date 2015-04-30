f4011d3eeabbeddf275a5d42b394021624b6e6c9
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-30 16:57:20 -0400

    Applied basic styling to the hermit poem

--- source/javascripts/conversation.js
+++ source/javascripts/conversation.js
@@ -2,12 +2,28 @@ $(document).on('ready', function(){
 
 	   if ($("body.the_hermit").length > 0) {
 
+	   		startUp();
 	   		$("#form").submit(function(event){
 	   			event.preventDefault();
 	   			var userText = $('#input').val();
+	   			$('.conversation > .message').append("<div class='user-container'><div class='user'>" + userText + "</div></div>");
+	   			response(userText);
 
-	   			$('.conversation > .user').append("<div>" + userText + "</div>");
 	   			$('#input').val("");
 	   		});
-	   }
+		}
+
+		function response(input){
+			if(input == "poem") {
+				$('.conversation > .message').append("<div class='sender'>" + "poem" + "</div>");
+			} else if (input == "video"){
+				$('.conversation > .message').append("<div class='sender'>" + "video" + "</div>");
+			} else {
+				$('.conversation > .message').append("<div class='sender'>" + "Not Valid" + "</div>");
+			}
+		}
+
+		function startUp(){
+			$('.conversation > .message').append("<div class='sender'>" + "Welcome to the poem content management system! Kind regards from your hosts of sprocket v2345.003 of the digital LAN system. " + "</div>");
+		}
 });
