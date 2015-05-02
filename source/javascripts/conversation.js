21c8c4c550d9b5ac8cb7db42336311698ecd5e1e
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-02 15:15:17 -0400

    Images appear in background, title styling

--- source/javascripts/conversation.js
+++ source/javascripts/conversation.js
@@ -29,28 +29,40 @@ $(document).on('ready', function(){
 				var part2Template = $('#part2Template').html();
 				Mustache.parse(part2Template);   // optional, speeds up future uses
 				var rendered = Mustache.render(part2Template);
-				$('.conversation > .message').append(rendered);	
+				$('.conversation > .message').append(rendered);
+
+				$("#video-container-hr").html($("<video class='hermit-bg-vid' autoplay loop muted />"));
+				$(".hermit-bg-vid").attr('src', 'http://s3.amazonaws.com/sonofsnow/videos/hermit2.mov');	
 
 			} else if (counter == 2){
 
 				var part3Template = $('#part3Template').html();
 				Mustache.parse(part3Template);   // optional, speeds up future uses
 				var rendered = Mustache.render(part3Template);
-				$('.conversation > .message').append(rendered);	
+				$('.conversation > .message').append(rendered);
+
+				$("#video-container-hr").html($("<video class='hermit-bg-vid' autoplay loop muted />"));
+				$(".hermit-bg-vid").attr('src', 'http://s3.amazonaws.com/sonofsnow/videos/hermit3.mov');		
 
 			} else if (counter == 3){
 
 				var part4Template = $('#part4Template').html();
 				Mustache.parse(part4Template);   // optional, speeds up future uses
 				var rendered = Mustache.render(part4Template);
-				$('.conversation > .message').append(rendered);	
+				$('.conversation > .message').append(rendered);
+
+				$("#video-container-hr").html($("<video class='hermit-bg-vid' autoplay loop muted />"));
+				$(".hermit-bg-vid").attr('src', 'http://s3.amazonaws.com/sonofsnow/videos/hermit4.mov');	
 				
 			} else if (counter == 4){
 
 				var part5Template = $('#part5Template').html();
 				Mustache.parse(part5Template);   // optional, speeds up future uses
 				var rendered = Mustache.render(part5Template);
-				$('.conversation > .message').append(rendered);	
+				$('.conversation > .message').append(rendered);
+
+				$("#video-container-hr").html($("<video class='hermit-bg-vid' autoplay loop muted />"));
+				$(".hermit-bg-vid").attr('src', 'http://s3.amazonaws.com/sonofsnow/videos/hermit5.mov');	
 
 			} else if (counter == 5){
 
@@ -58,6 +70,9 @@ $(document).on('ready', function(){
 				Mustache.parse(part6Template);   // optional, speeds up future uses
 				var rendered = Mustache.render(part6Template);
 				$('.conversation > .message').append(rendered);
+
+				$("#video-container-hr").html($("<video class='hermit-bg-vid' autoplay loop muted />"));
+				$(".hermit-bg-vid").attr('src', 'http://s3.amazonaws.com/sonofsnow/videos/hermit6.mov');
 
 			} else {
 				console.log("This worked");
@@ -70,6 +85,9 @@ $(document).on('ready', function(){
 			Mustache.parse(part1Template);   // optional, speeds up future uses
 			var rendered = Mustache.render(part1Template);
 			$('.conversation > .message').append(rendered);
+
+			$("#video-container-hr").append($("<video class='hermit-bg-vid' autoplay loop muted />"));
+				$(".hermit-bg-vid").attr('src', 'http://s3.amazonaws.com/sonofsnow/videos/hermit1.mov');
 		}
 
 		function alternateResponses(input) {
