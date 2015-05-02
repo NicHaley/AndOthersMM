e567bb4e5d3d82dbd5cdf627c76a96f3d8289a1f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-05-02 13:56:01 -0400

    Got questions/responses working

--- source/javascripts/conversation.js
+++ source/javascripts/conversation.js
@@ -1,29 +1,83 @@
 $(document).on('ready', function(){
 
+	var responseTime = 1000;
+	var counter = 0;
+
 	   if ($("body.the_hermit").length > 0) {
 
 	   		startUp();
 	   		$("#form").submit(function(event){
 	   			event.preventDefault();
+	   			counter++;
+
 	   			var userText = $('#input').val();
 	   			$('.conversation > .message').append("<div class='user-container'><div class='user'>" + userText + "</div></div>");
-	   			response(userText);
+
+	   			
+	   			setTimeout(function(){
+	   				response(userText);
+	   			}, responseTime);
 
 	   			$('#input').val("");
 	   		});
 		}
 
 		function response(input){
-			if(input == "poem") {
-				$('.conversation > .message').append("<div class='sender'>" + "poem" + "</div>");
-			} else if (input == "video"){
-				$('.conversation > .message').append("<div class='sender'>" + "video" + "</div>");
+
+			if (counter == 1){
+
+				var part2Template = $('#part2Template').html();
+				Mustache.parse(part2Template);   // optional, speeds up future uses
+				var rendered = Mustache.render(part2Template);
+				$('.conversation > .message').append(rendered);	
+
+			} else if (counter == 2){
+
+				var part3Template = $('#part3Template').html();
+				Mustache.parse(part3Template);   // optional, speeds up future uses
+				var rendered = Mustache.render(part3Template);
+				$('.conversation > .message').append(rendered);	
+
+			} else if (counter == 3){
+
+				var part4Template = $('#part4Template').html();
+				Mustache.parse(part4Template);   // optional, speeds up future uses
+				var rendered = Mustache.render(part4Template);
+				$('.conversation > .message').append(rendered);	
+				
+			} else if (counter == 4){
+
+				var part5Template = $('#part5Template').html();
+				Mustache.parse(part5Template);   // optional, speeds up future uses
+				var rendered = Mustache.render(part5Template);
+				$('.conversation > .message').append(rendered);	
+
+			} else if (counter == 5){
+
+				var part6Template = $('#part6Template').html();
+				Mustache.parse(part6Template);   // optional, speeds up future uses
+				var rendered = Mustache.render(part6Template);
+				$('.conversation > .message').append(rendered);
+
 			} else {
-				$('.conversation > .message').append("<div class='sender'>" + "Not Valid" + "</div>");
+				console.log("This worked");
+				alternateResponses(input);
 			}
 		}
 
-		function startUp(){
-			$('.conversation > .message').append("<div class='sender'>" + "Welcome to the poem content management system! Kind regards from your hosts of sprocket v2345.003 of the digital LAN system. " + "</div>");
+		function startUp (){
+			var part1Template = $('#part1Template').html();
+			Mustache.parse(part1Template);   // optional, speeds up future uses
+			var rendered = Mustache.render(part1Template);
+			$('.conversation > .message').append(rendered);
+		}
+
+		function alternateResponses(input) {
+			if (input == "yolo") {
+				alert("get out of my house");
+			} else {
+				console.log("altres else worked");
+				alert("suck a dick");
+			}
 		}
 });
